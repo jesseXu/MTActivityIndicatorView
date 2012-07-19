ca7d68083d5ffedb0d21824d4b21b738927c7a16
jesse <jesse0628@gmail.com>
2012-07-19 10:24:08 +0800

    Initial Commit

---
+++ MTActivityIndicatorView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MTActivityIndicatorView
+//
+//  Created by jesse on 12-7-19.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MTActivityIndicatorView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MTActivityIndicatorView
+//
+//  Created by jesse on 12-7-19.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
